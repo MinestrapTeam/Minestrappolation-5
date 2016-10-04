6e6c158e772358ee0a8060cb842c52d695e354b4
Delocuro <delocuro@gmail.com>
2016-10-04 00:26:13 -0400

    Revert "Fucked my workspace up, reverting this commit because github sucks"
    This reverts commit 9664cfd0452ff9d19fcc7d8e748a504d3043e393.

--- src/main/java/minestrapp/blocks/BlockColdSand.java
+++ src/main/java/minestrapp/blocks/BlockColdSand.java
@@ -3,6 +3,7 @@ package minestrapp.blocks;
 import java.util.List;
 
 import minestrapp.blocks.utility.BlockBase;
+import minestrapp.blocks.utility.GravityBlockBase;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
@@ -17,7 +18,7 @@ import net.minecraft.util.IStringSerializable;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockColdSand extends BlockBase
+public class BlockColdSand extends GravityBlockBase
 {
 	public static final PropertyEnum<BlockColdSand.EnumType> VARIANT = PropertyEnum.<BlockColdSand.EnumType>create("variant", BlockColdSand.EnumType.class);
 	
